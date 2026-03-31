5b8a1e1641311ac63fd5032d6c4db11a79e4d3d8
mikael <mikaelvo639@gmail.com>
2026-03-31 21:04:31 +0700

    Add MoveAnimation and SwapAnimation classes for node animations

---
+++ src/UI/Animations/SwapAnimation.cpp
@@ -0,0 +1,48 @@
+#include "UI/Animations/SwapAnimation.hpp"
+#include <algorithm>
+#include <cmath> 
+
+namespace UI::Animations{
+
+    SwapAnimation::SwapAnimation(UI::DSA::Node* a, UI::DSA::Node* b, float duration)
+            : nodeA(a), nodeB(b), totalDuration(duration), elapsedTime(0.f) 
+    {
+        if (nodeA && nodeB) {
+            startPosA = nodeA->getPosition();
+            startPosB = nodeB->getPosition();
+        }
+    }
+
+    void SwapAnimation::update(float dt) {
+            if (!nodeA || !nodeB || isFinished()) return;
+
+            elapsedTime += dt;
+            float t = std::min(elapsedTime / totalDuration, 1.0f);
+
+            // Base Lerp
+            sf::Vector2f lerpA = startPosA + t * (startPosB - startPosA);
+            sf::Vector2f lerpB = startPosB + t * (startPosA - startPosB);
+
+            
+            sf::Vector2f diff = startPosB - startPosA;
+            float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
+            
+            if (distance > 0.001f) {
+                // normal vector
+                sf::Vector2f normal(-diff.y / distance, diff.x / distance);
+
+                // some math 🥀
+                float arcHeight = std::sin(t * PI) * (distance / 2.0f);
+
+                sf::Vector2f currentPosA = lerpA + normal * arcHeight;
+                sf::Vector2f currentPosB = lerpB - normal * arcHeight;
+
+                nodeA->setPosition(currentPosA);
+                nodeB->setPosition(currentPosB);
+            }
+        }
+
+    bool SwapAnimation::isFinished() const {
+            return elapsedTime >= totalDuration;
+        }
+}
