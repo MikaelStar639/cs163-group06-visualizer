c4b31b164963634aaa47b8f5d16261d45abd8b7e
mikael <mikaelvo639@gmail.com>
2026-04-08 21:23:52 +0700

    Add Layout Engine for Linked List

---
+++ include/UI/DSA/LayoutEngine.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "UI/DSA/Graph.hpp"
+#include "UI/Animations/Core/ParallelAnimation.hpp"
+#include <memory>
+
+namespace UI::DSA::LayoutEngine {
+
+    // Linked list:
+    // Returns a group of animation contains all of the Node moves.
+    // startX, startY: Coordinate of the first node
+    // spacing: space between nodes
+    std::unique_ptr<Animations::ParallelAnimation> asLinkedList(
+        const Graph& graph, 
+        float startX, 
+        float startY, 
+        float spacing, 
+        float duration = 0.5f
+    );
+
+    // Heap:
+    // ...
+
+    // Trie:
+    // ...
+
+    // MST:
+    // ...
+} // namespace UI::DSA::LayoutEngine
